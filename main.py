0bee7ab7a9cb37d129f7a1b6d9cbb0a8b790020a
aykutcayir34 <aykut.cayir@khas.edu.tr>
2024-07-30 16:10:24 +0300

    initial commit.

---
+++ main.py
@@ -0,0 +1,97 @@
+#%%
+words = open('names.txt', 'r').read().splitlines()
+# %%
+words[:10]
+# %%
+len(words)
+# %%
+min(len(w) for w in words)
+# %%
+max(len(w) for w in words)
+
+# %%
+b = {}
+for w in words:
+    chs = ['.'] + list(w) + ['.']
+    for ch1, ch2 in zip(chs, chs[1:]):
+        bigram = (ch1, ch2)
+        b[bigram] = b.get(bigram, 0) + 1
+
+# %%
+b
+# %%
+sorted(b.items(), key=lambda x: -x[1])
+# %%
+import torch
+# %%
+N = torch.zeros((27 ,27), dtype=torch.int32)
+# %%
+chars = sorted(list(set(''.join(words))))
+stoi = {s: i+1 for i, s in enumerate(chars)}
+stoi['.'] = 0
+stoi
+# %%
+for w in words:
+    chs = ['.'] + list(w) + ['.']
+    for ch1, ch2 in zip(chs, chs[1:]):
+        xi1 = stoi[ch1]
+        xi2 = stoi[ch2]
+        N[xi1, xi2] += 1
+# %%
+import matplotlib.pyplot as plt
+# %%
+
+# %%
+itos = {i:s for s, i in stoi.items()}
+itos
+# %%
+plt.figure(figsize=(16, 16))
+plt.imshow(N, cmap='Blues')
+for i in range(27):
+    for j in range(27):
+        chstr = itos[i] + itos[j]
+        plt.text(j, i, chstr, ha='center', va='bottom', color='gray')
+        plt.text(j, i, N[i, j].item(), ha='center', va='top', color='gray')
+plt.axis('off')
+# %%
+N[0]
+# %%
+p = N[0].float()
+p /= p.sum()
+p
+# %%
+g = torch.Generator().manual_seed(2147483647)
+ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
+itos[ix]
+#%%
+P = N.float()
+P /= P.sum(dim=1, keepdim=True)
+P
+# %%
+g = torch.Generator().manual_seed(2147483647)
+for i in range(5):
+    out = []
+    ix = 0
+    while True:
+        p = P[ix]
+        ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
+        out.append(itos[ix])
+        if ix == 0:
+            break
+    print(''.join(out))
+# %%
+log_likelihood = 0.0
+for w in words[:3]:
+    chs = ['.'] + list(w) + ['.']
+    for ch1, ch2 in zip(chs, chs[1:]):
+        xi1 = stoi[ch1]
+        xi2 = stoi[ch2]
+        prob = P[xi1, xi2]
+        logprob = torch.log(prob)
+        log_likelihood += logprob
+        print(f'{ch1}{ch2}: {prob:.4f} {logprob:.4f}')
+print(f'{log_likelihood.item():.4f}')
+# %%
+nll = -log_likelihood
+print(f'{nll.item():.4f}')
+# %%
